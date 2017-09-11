73ccb6a3f7ac3584b09faf78a93dd6a5b6e9abda
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2017-09-11 11:03:18 +0200

    Implement registration form stub in PHP

---
+++ registration.php
@@ -0,0 +1,80 @@
+<?php
+
+require 'src/form.php';
+
+$registration_form = new Form();
+$registration_form->add(new FormTextField('first_name', 'First name', ['required' => true]));
+$registration_form->add(new FormTextField('last_name', 'Last name', ['required' => true]));
+$registration_form->add(new FormEmailField('email', 'Email address', ['required' => true]));
+
+$registration_form->add(new FormSelectField('register_as', 'Registering as', [
+							'student' => 'BSc or MSc student',
+							'phd' => 'PhD student',
+							'regular' => 'Regular'
+						], ['required' => true]));
+
+$errors = $registration_form->submitted() ? $registration_form->validate() : [];
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Register &middot; BNAIC 2017</title>
+		<meta name="description" content="The 29th Benelux Conference on Artificial Intelligence">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" href="assets/css/layout.css">
+	</head>
+	<body>
+		<header class="website-header small">
+			<div class="text">
+				<div class="container">
+					<h1>BNAIC 2017</h1>
+					<p>The 29th Benelux Conference on Artificial Intelligence</p>
+					<p>November 8&ndash;9, 2017 in Groningen, The Netherlands</p>
+				</div>
+			</div>
+		</header>
+
+		<nav class="website-navigation fixed" tabindex="-1">
+			<div class="container">
+				<a href="index.html#home">Home</a>
+				<a href="cfp.html">Call for papers</a>
+				<a href="committees.html">Committees</a>
+				<!-- <a href="important-dates.html">Important dates</a> -->
+				<a href="location.html">Location</a>
+				<a href="registration.php" class="registration active">Registration</a>
+			</div>
+		</nav>
+
+		<div class="website-content">
+			<div class="container">
+				<section>
+					<h1>Registration</h1>
+					<p>Something about the various costs for the various types of visitors, e.g. cheap for students, expensive for people who's supervisor/boss is paying anyway.</p> 
+				</section>
+				<section>
+					<form method="post" action="registration.php">
+						<h3>Personal</h3>
+						<?= $registration_form->first_name->render($errors) ?>
+						<?= $registration_form->last_name->render($errors) ?>
+						<?= $registration_form->email->render($errors) ?>
+
+						<h3>Relation</h3>
+						<?= $registration_form->register_as->render($errors) ?>
+	
+						<div class="form-controls">
+							<button type="submit">Submit registration</button>
+						</div>
+					</form>
+				</section>
+			</div>
+		</div>
+
+		<footer>
+			<div class="container">
+				<p>Contact: <a href="mailto:Elina Sietsema &lt;e.sietsema@rug.nl&gt;">Elina Sietsema (e.sietsema@rug.nl)</a></p>
+			</div>
+		</footer>
+	</body>
+</html>
---
+++ src/form.php
@@ -0,0 +1,155 @@
+<?php
+
+class Form
+{
+	public $fields = [];
+
+	public function add(FormField $field)
+	{
+		$this->fields[$field->name()] = $field;
+	}
+
+	public function __get($field_name)
+	{
+		return $this->fields[$field_name];
+	}
+
+	public function submitted()
+	{
+		return $_SERVER['REQUEST_METHOD'] == 'POST';
+	}
+
+	public function validate()
+	{
+		$errors = [];
+
+		foreach ($this->fields as $name => $field) {
+			$error = $field->validate();
+
+			if ($error !== null)
+				$errors[$name] = $error;
+		}
+
+		return $errors;
+	}
+}
+
+abstract class FormField
+{
+	protected $attributes = [];
+
+	public function __construct($name, $label, array $attributes = [])
+	{
+		$this->name = $name;
+
+		$this->label = $label;
+
+		$this->attributes = array_merge($attributes, ['name' => $name, 'id' => 'field-' . $name]);
+	}
+
+	public function name()
+	{
+		return $this->name;
+	}
+
+	public function value()
+	{
+		return isset($_POST[$this->name]) ? trim($_POST[$this->name]) : null;
+	}
+
+	public function validate()
+	{
+		if (!empty($attributes['required']) && $this->value() == '')
+			return 'This field cannot be left empty';
+
+		return null;
+	}
+
+	abstract public function render(array &$errors = []);
+
+	protected function render_attributes(array $attributes)
+	{
+		$pairs = [];
+
+		foreach ($attributes as $prop => $value)
+			$pairs[] = ($value === true)
+				? $prop // HTML5 name only attributes, e.g. 'required' and 'checked'
+				: sprintf('%s="%s"', $prop, htmlspecialchars($value, ENT_QUOTES));
+
+		return implode(' ', $pairs);
+	}
+
+	protected function render_group($field, &$error = null)
+	{
+		$label = sprintf('<label for="%s">%s:</label>', $this->attributes['id'], htmlspecialchars($this->label));
+
+		$hint = !empty($error)
+			? sprintf('<p class="hint">%s</p>', htmlspecialchars($error))
+			: '';
+
+		return sprintf('<div class="form-group%s">%s%s%s</div>',
+			!empty($this->attributes['required']) ? ' required' : '',
+			$label,
+			$field,
+			$hint);	
+	}
+}
+
+class FormTextField extends FormField 
+{	
+	protected $default_attributes = [
+		'type' => 'text'
+	];
+
+	public function render(array &$errors = null)
+	{
+		$attributes = array_merge($this->default_attributes, $this->attributes);
+
+		if ($this->value() !== null)
+			$attributes = array_merge($attributes, ['value' => $this->value()]);
+
+		$field = sprintf('<input %s>', $this->render_attributes($attributes));
+
+		return $this->render_group($field, $errors[$this->name]);
+	}
+}
+
+class FormEmailField extends FormTextField
+{
+	protected $default_attributes = [
+		'type' => 'email'
+	];
+
+	public function validate()
+	{
+		if (filter_var($this->value(), FILTER_VALIDATE_EMAIL) === false)
+			return 'This email address does not seem to be valid';
+
+		return parent::validate();
+	}
+}
+
+class FormSelectField extends FormField
+{
+	public function __construct($name, $label, array $options, array $attributes = [])
+	{
+		parent::__construct($name, $label, $attributes);
+
+		$this->options = $options;
+	}
+
+	public function render(array &$errors = [])
+	{
+		$html_options = [];
+
+		foreach ($this->options as $option_value => $option_label)
+			$html_options[] = sprintf('<option%s value="%s">%s</option>',
+				$this->value() == $option_value ? ' selected' : '',
+				htmlspecialchars($option_value, ENT_QUOTES),
+				htmlspecialchars($option_label));
+
+		$field = sprintf('<select %s>%s</select>', $this->render_attributes($this->attributes), implode("\n\t", $html_options));
+
+		return $this->render_group($field, $errors[$this->name]);
+	}
+}
